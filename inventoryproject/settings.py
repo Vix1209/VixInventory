18db51de023819d02cacd8a0ec33247d69285e1b
Uchenna Ofor <ucheofor2015@gmail.com>
2023-04-10 07:54:38 +0100

    database changed

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -77,13 +77,25 @@ WSGI_APPLICATION = 'inventoryproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'HOST': ['vixinventory.onrender.com', 'localhost']
+#     }
+# }
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' : {
+        'ENGINE' :  'django.db.backends.postgresql_psycopg2',
+        'NAME' :    'vixinventory',
+        'USER' :    'postgres',
+        'PASSWORD': 'Ucvickazzy01@#/...',
+        'HOST' :    'localhost',
+        'PORT' :    '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
