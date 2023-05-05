f3b2b2b7fea5855286f905da681cef81ed5a6fbb
Uchenna Ofor <ucheofor2015@gmail.com>
2023-05-05 08:32:31 +0100

    whitenoise added

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', #this solves the issue of mimetype
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
