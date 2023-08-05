83b3d1a6dbd43e9f7bce55614690b4a909208eb8
Uchenna Ofor <ucheofor2015@gmail.com>
2023-08-05 06:44:41 +0100

    tweaks made again and again and again

--- inventoryproject/settings.py
+++ inventoryproject/settings.py
@@ -78,25 +78,25 @@ WSGI_APPLICATION = 'inventoryproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#         'HOST': ['vixinventory.onrender.com', 'localhost']
-#     }
-# }
-
-
 DATABASES = {
-    'default' : {
-        'ENGINE' :  'django.db.backends.postgresql_psycopg2',
-        'NAME' :    'vixinventory',
-        'USER' :    'postgres',
-        'PASSWORD': 'Ucvickazzy01@#/...',
-        'HOST' :    'localhost',
-        'PORT' :    '5432',
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'HOST': ['localhost']
     }
 }
+
+
+# DATABASES = {
+#     'default' : {
+#         'ENGINE' :  'django.db.backends.postgresql_psycopg2',
+#         'NAME' :    'vixinventory',
+#         'USER' :    'postgres',
+#         'PASSWORD': 'Ucvickazzy01@#/...',
+#         'HOST' :    'localhost',
+#         'PORT' :    '5432',
+#     }
+# }
 
 
 # ## setting up the environment variables
